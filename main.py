da54996f7c538ad7de1daed9f1a2e3051b93d120
simon <simony.jp@hotmail.com>
2025-03-23 11:06:36 -0400

    creading main loop connecting class manu and gae classes

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.game import Game
 
-pygame.init()
-print('Setup Start')
-window = pygame.display.set_mode(size=(600, 480))
-print('Setup End')
-
-print('Loop Start')
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # Close Window
-            quit()  # End pypgame
+game = Game()
+game.run()
