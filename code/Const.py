8ff9e43f0bb4d8d1b74dd1c3a0fff663786ea129
simon <simony.jp@hotmail.com>
2025-03-23 20:12:59 -0400

    creading main loop connecting class manu and gae classes

---
+++ code/Const.py
@@ -0,0 +1,16 @@
+#C
+COLOR_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
+
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+# W
+WIN_WIDTH = 576
+WIND_HEIGHT = 324
